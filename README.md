73d1093a85dda210e767af4e568003c06bfea21d
cake <mail@mail.com>
2024-02-29 17:34:56 +0200

    Improve readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Apt dependency list
 
 List all of the dependencies of an apt package, recursively
+
+## Usage
+
+```sh
+./dep_lister.py $PACKAGE_NAME
+```
+
+Will generate `${PACKAGE_NAME}_deps.json`
--- dep_lister.py
+++ dep_lister.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import subprocess
 import shlex
 import re
@@ -8,22 +9,24 @@ import click
 
 DEP_PATTERN = re.compile(r'Inst (\S+) \((\S+).+\)')
 
+
 def list_deps(package: str) -> List[Tuple[str, str]]:
     proc = subprocess.Popen(['docker', 'run', '--rm', 'ubuntu:20.04', 'sh', '-c', f'apt update && apt install --dry-run -y {package}'], stdout=subprocess.PIPE)
     deps = []
     for line in proc.stdout:
-        print(line.decode())
+        print(line.decode(),end='')
         match = DEP_PATTERN.match(line.decode())
         if match is not None:
             deps.append(match.groups())
     proc.wait()
     return deps
 
+
 @click.command()
 @click.argument("package", required=True)
 def main(package):
     deps = list_deps(package)
-    with open(f'{pakcage}_deps.json', 'w') as out:
+    with open(f'{package}_deps.json', 'w') as out:
         json.dump(deps, out)
                 
 
